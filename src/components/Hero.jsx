20fada80b4881a8575caf397fb9e7b804fab9300
Sharad Jadhav <60263979+jadhavsharad@users.noreply.github.com>
2024-02-26 23:14:10 +0530

    updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from 'react'
 import { Parallax } from 'react-scroll-parallax';
 import { TypeAnimation } from 'react-type-animation';
-import GridLines from 'react-gridlines';
+// import GridLines from 'react-gridlines';
 import HeroElements from '../utils/HeroElements'
+import '../utils/Locomotive.css'
+import LocomotiveScroll from 'locomotive-scroll';
+
+new LocomotiveScroll();
 
 
 
@@ -25,17 +29,18 @@ const Hero = () => {
 
   return (
     <>
-      <div className='text-[#fdf5eb] h-screen '>
-        <GridLines className="w-full h-full flex flex-col justify-center items-center relative" cellWidth={190} cellHeight={80} strokeWidth={2} lineColor={"#191919"}>
+      <div data-scroll-container className='text-[#fdf5eb] h-screen overflow-hidden'>
+      <div className="w-full min-h-full bg-[#090909] bg-grid-[#222] relative flex flex-col items-center justify-center">
+          <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-inherit [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
           <Parallax scale={[2, 0.5]} opacity={[2, 0]} className='flex text-8xl font-Figtree lowercase font-semibold'>
             <div className="">&lt;</div>
             <div className="text-[#fdf5eb]">
               /<TypeAnimation className='select-none' sequence={['Software', 2000, 'Developer', 2000,]} speed={20} wrapper="span" cursor={false} repeat={Infinity} />
             </div>
-          <div>&gt;</div>
+            <div>&gt;</div>
           </Parallax>
-          <HeroElements/>
-        </GridLines>
+          <HeroElements data-scroll />
+        </div>
       </div>
     </>
   )
